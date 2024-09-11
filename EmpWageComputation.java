b092494347955d0bd84e900322c1aa43414829b9
likhithashylu <likitha.shylu@gmail.com>
2024-09-11 13:13:56 +0530

    Adding Functionalities to Calculate Wages Per Hour

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,34 +2,34 @@ class EmpWageComputation {
 	public static void main(String[] args) {
 		System.out.println("===== WELCOME TO EMPLOYEE WAGE COMPUTATION ======");
 		System.out.println("");
-		  int wagePerHour = 20;
-	        int fullDay = 8;
-	        int halfDay = 4;
+		int wagePerHour = 20;
+		int fullDay = 8;
+		int halfDay = 4;
+		int totalNoOfDays = 20;
 
-	        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-	        switch ( empCheck ) {
-	            case 0:
-	                System.out.print("Employee is Present");
-	                System.out.println("");
-	                System.out.print("The daily wage of Employee is : " + (wagePerHour * fullDay));
-	                System.out.println("");
-	                break;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch ( empCheck ) {
+		case 0:
+			System.out.print("Employee is Present");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + ( wagePerHour * fullDay * totalNoOfDays ));
+			System.out.println("");
+			break;
 
-	            case 1:
-	                System.out.print("Employee is Present but working part time ");
-	                System.out.println("");
-	                System.out.print("The daily wage of Employee is : " + (wagePerHour * halfDay));
-	                System.out.println("");
-	                break;
+		case 1:
+			System.out.print("Employee is Present but working part time ");
+			System.out.println("");
+			System.out.print("The daily wage of Employee is : " + ( wagePerHour * halfDay * totalNoOfDays ));
+			System.out.println("");
+			break;
 
-	            case 2:
-	                System.out.print("Employee is Absent");
-	                System.out.println("");
-	                System.out.println("The daily wage of Employee is : 0 ");
-	                System.out.println("");
-	                break;
+		case 2:
+			System.out.print("Employee is Absent");
+			System.out.println("");
+			System.out.println("The daily wage of Employee is : 0 ");
+			System.out.println("");
+			break;
 
-	        }
-
+		}
 	}
 }
